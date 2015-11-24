d9cd412639804a18c73f4564a0a99d0b08b3d89f
zhenglianfu <15067134439@163.com>
2015-11-24 22:00:22 +0800

    create promise and defer the core of async program

--- scripts/site.js
+++ scripts/site.js
@@ -1,3 +1,23 @@
 (function(){
+	var createXHR = function(){
+		if (window.XMLHttpRequest) {
+			return new XMLHttpRequest;	
+		} else if (window.ActiveXObject) {
+			return new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		return null;	
+	};
+	// 实现promise， 不能在外部访问状态机
+	function Promise(){
+		this.listeners = [];
+		this.length = 0;	
+	};
+	Promise.prototype = {
+		done: function(){},
+		promise: function(){}	
+	};
+	// defer 
+	function defer(){
 		
+	}
 }());
