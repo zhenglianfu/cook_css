2e68c5d9b8df731d8e133a3b21c962af221866ad
zhenglianfu <15067134439@163.com>
2015-11-25 19:13:58 +0800

    update nothing

--- scripts/site.js
+++ scripts/site.js
@@ -7,17 +7,4 @@
 		}
 		return null;	
 	};
-	// 实现promise， 不能在外部访问状态机
-	function Promise(){
-		this.listeners = [];
-		this.length = 0;	
-	};
-	Promise.prototype = {
-		done: function(){},
-		promise: function(){}	
-	};
-	// defer 
-	function defer(){
-		
-	}
 }());
