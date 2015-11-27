75259b09a188e5e042c1bfa2cd2e70196b521f67
zhenglianfu <15067134439@163.com>
2015-11-27 19:42:35 +0800

    update

--- scripts/promise.js
+++ scripts/promise.js
@@ -8,13 +8,35 @@
 	}
 }(this, function(exports){
 	// 实现promise， 不能在外部访问状态机
-	function Promise(){
-		this.listeners = [];
-		this.length = 0;	
+	function Promise(ins){
+		var origin = ins || {};
+		// local and private variable of Promise
+		var resolved = false;
+		var callbacks = [];
+		var length = 0;
+		this.status = function(){
+			return resolved;
+		};
+		this.reject = function(){
+			resolved = false;
+		}
+		// resolve只能被调用一次，之后加入的handler会被立即执行。
+		// [状态机只能变化一次，无法重置]
+		this.resolve = function(){
+			this.resolve = function(){};
+			resolved = true;			
+		};
+		this.then = function(fn){
+			typeof fn === 'function' ? (callbacks[length ++] = fn) : 0;
+			return this;
+		};
+		return this;
 	};
 	Promise.prototype = {
-		done: function(){},
-		promise: function(){}	
+		done: function(fn){
+			this.status() ? (typeof fn === 'function' && fn()) : this.then(fn);
+			return this;
+		}
 	};
 	// defer 
 	function defer(){
